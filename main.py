4f49db7eb665d8ae3176f202d23d30d4531d7d3d
ChristianHesberg <chri59kx@easv365.dk>
2023-06-09 11:13:22 +0200

    added more functionality

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 
 
+
 def getItemById(id):
     r = requests.get('https://prices.runescape.wiki/api/v1/osrs/latest?id=' + str(id),
                      headers={'Accept': 'application/json',
@@ -42,49 +43,20 @@ def calculateMarginCollection(dictionaryOfItems):
     listOfItems = []
     for item in dictionaryOfItems:
         margin = calculateMarginSingle(item["id"])
-        listOfItems.append((item["name"], margin))
+        listOfItems.append((item["id"], item["name"], margin))
     sortedList = sorted(listOfItems, key=itemgetter(1), reverse=True)
     print(sortedList)
+    return sortedList
 
+def getTimeSeriesFromList(listOfItems):
+    listOfSeries = []
+    for item in listOfItems:
+        listOfSeries.append((item[1], item[2], getTimeSeriesById(item[0])))
+    return listOfSeries
 
-def calculateVolatility(id):
-    series = getTimeSeriesById(id)
-    high = []
-    low = []
-    lastHigh = 0
-    lastLow = 0
-    for item in series:
-        if(item['avgHighPrice'] != None):
-            high.append(item['avgHighPrice'] - lastHigh)
-            lastHigh = item['avgHighPrice']
-        if(item['avgLowPrice'] != None):
-            low.append(item['avgLowPrice'] - lastLow)
-            lastLow = item['avgLowPrice']
-    del low[0]
-    del high[0]
-    dataLow = low[-10:]
-    dataHigh = high[-10:]
-    lowVolatilityHalf = np.std(dataLow)
-    highVolatilityHalf = np.std(dataHigh)
-    lowVolatility = np.std(low)
-    highVolatility = np.std(high)
-    print(low)
-    print(high)
-    print(highVolatility)
-    print(lowVolatility)
-    print(highVolatilityHalf)
-    print(lowVolatilityHalf)
-    #print(lowChange)
-
-def pandasTest(id):
-    series = getTimeSeriesById(id)
-    frame = pd.DataFrame(series)
-    print(frame)
-
-def priceCalc(id):
+def filterOutliers(series):
     # Assuming 'data' is a Pandas DataFrame with 'buy_price' and 'sell_price' columns
-    series = getTimeSeriesById(id)
-    data = pd.DataFrame(series[-11:])
+    data = pd.DataFrame(series[-24:])
     print(data)
     threshhold = .3
 
@@ -95,39 +67,26 @@ def priceCalc(id):
     outlier_threshold = threshhold * iqr
 
     # Filter outliers for buy price
-    filtered_data = data[
+    filtered_data_buy = data[
         (data['avgLowPrice'] >= (q1 - outlier_threshold)) & (data['avgLowPrice'] <= (q3 + outlier_threshold))]
-    print(filtered_data)
-
-    # Determine good buy price
-    good_buy_price = filtered_data['avgLowPrice'].mean()
+    print(filtered_data_buy)
 
     # Filter outliers for sell price
     q1 = data['avgHighPrice'].quantile(0.25)
     q3 = data['avgHighPrice'].quantile(0.75)
     iqr = q3 - q1
     outlier_threshold = threshhold * iqr
-    filtered_data = data[
+    filtered_data_sell = data[
         (data['avgHighPrice'] >= (q1 - outlier_threshold)) & (data['avgHighPrice'] <= (q3 + outlier_threshold))]
-    print(filtered_data)
+    print(filtered_data_sell)
+    return filtered_data_buy, filtered_data_sell
 
-    # Determine good sell price
-    good_sell_price = filtered_data['avgHighPrice'].mean()
-
-    print("Good Buy Price:", good_buy_price)
-    print("Good Sell Price:", good_sell_price)
-
-
-def zSort(id):
-    import pandas as pd
-    import numpy as np
-
+def zSortFilterOutliers(series):
     # Assuming 'data' is a Pandas DataFrame with 'buy_price' and 'sell_price' columns
-    series = getTimeSeriesById(id)
-    data = pd.DataFrame(series[-11:])
+    data = pd.DataFrame(series[-24:])
     print(data)
     # Define outlier threshold using the Z-score method
-    z_threshold = 1
+    z_threshold = .85
 
     # Filter outliers for buy price
     z_scores_buy = np.abs((data['avgLowPrice'] - data['avgLowPrice'].mean()) / data['avgLowPrice'].std())
@@ -135,27 +94,51 @@ def zSort(id):
     filtered_data_buy = data[z_scores_buy <= z_threshold]
     print(filtered_data_buy)
 
-    # Determine good buy price
-    good_buy_price = filtered_data_buy['avgLowPrice'].mean()
-
     # Filter outliers for sell price
     z_scores_sell = np.abs((data['avgHighPrice'] - data['avgHighPrice'].mean()) / data['avgHighPrice'].std())
     print(z_scores_sell)
     filtered_data_sell = data[z_scores_sell <= z_threshold]
     print(filtered_data_sell)
-    # Determine good sell price
-    good_sell_price = filtered_data_sell['avgHighPrice'].mean()
+    return filtered_data_buy, filtered_data_sell
 
-    print("Good Buy Price:", good_buy_price)
-    print("Good Sell Price:", good_sell_price)
+def calculateVolatilityAbsMean(lists):
+    buyPriceList = lists[0]['avgLowPrice']
+    sellPriceList = lists[1]['avgHighPrice']
+
+    buyDiff = buyPriceList.diff()
+    sellDiff = sellPriceList.diff()
+
+    print(buyDiff)
+    print(sellDiff)
+
+    buyVolatility = buyDiff.abs().mean()
+    sellVolatility = sellDiff.abs().mean()
+
+    print(buyVolatility)
+    print(sellVolatility)
+
+
+def calculateMean(lists):
+    buyPriceListMean = lists[0]['avgLowPrice'].mean()
+    sellPriceListMean = lists[1]['avgHighPrice'].mean()
+    print(buyPriceListMean)
+    print(sellPriceListMean)
+    return buyPriceListMean, sellPriceListMean
+
+def calculateGoodFlips():
+    margins = calculateMarginCollection(itemList.items)
+    items = getTimeSeriesFromList(margins)
+
+    
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #zSort(13237)
-    priceCalc(27657)
-    #calculateVolatility(13237)
-    #pandasTest(13237)
-    # calculateMarginCollection(itemList.items)
+    #series = getTimeSeriesById(13239)
+    #lists = zSortFilterOutliers(series)
+    #calculateVolatilityAbsMean(lists)
+    #calculateMean(lists)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
