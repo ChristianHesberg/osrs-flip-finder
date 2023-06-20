a5af332e388b8130e73d7ac978080513f9ea559d
ChristianHesberg <chri59kx@easv365.dk>
2023-06-20 21:52:00 +0200

    ...

--- itemList.py
+++ itemList.py
@@ -6,5 +6,16 @@ items = [
     {name: "basilisk jaw", id: 24268},
     {name: "dragon claws", id: 13652},
     {name: "armadyl crossbow", id: 11785},
+    {name: "saradomin godsword", id: 11806},
+    {name: "kodai wand", id: 21006},
+    {name: "ghrazi rapier", id: 22324},
+    {name: "osmumten's fang", id: 26219},
+    {name: "webweaver bow", id: 27652},
+    {name: "ursine chainmace", id: 27657},
+    {name: "avernic defender", id: 22477},
+    {name: "masori body(f)", id: 27238},
+    {name: "masori chaps(f)", id:27241},
+    {name: "masori mask(f)", id: 27235},
+    {name: "zaryte crossbow", id:26374},
 
 ]
--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def calculateMarginSingle(id):
     high = prices["high"]
     low = prices["low"]
     margin = high - low - high * 0.01
-    return margin
+    return high, low, margin
 
 
 def calculateMarginCollection(dictionaryOfItems):
@@ -45,20 +45,21 @@ def calculateMarginCollection(dictionaryOfItems):
         margin = calculateMarginSingle(item["id"])
         listOfItems.append((item["id"], item["name"], margin))
     sortedList = sorted(listOfItems, key=itemgetter(1), reverse=True)
-    print(sortedList)
     return sortedList
 
 def getTimeSeriesFromList(listOfItems):
     listOfSeries = []
     for item in listOfItems:
-        listOfSeries.append((item[1], item[2], getTimeSeriesById(item[0])))
+        print(item)
+        listOfSeries.append((item[0], item[1], item[2], getTimeSeriesById(item[0])))
     return listOfSeries
 
 def filterOutliers(series):
+    amount5MinuteSlicesBack = -24
     # Assuming 'data' is a Pandas DataFrame with 'buy_price' and 'sell_price' columns
-    data = pd.DataFrame(series[-24:])
+    data = pd.DataFrame(series[amount5MinuteSlicesBack:])
     print(data)
-    threshhold = .3
+    threshhold = .1
 
     # Define outlier threshold using the interquartile range (IQR) method
     q1 = data['avgLowPrice'].quantile(0.25)
@@ -82,11 +83,12 @@ def filterOutliers(series):
     return filtered_data_buy, filtered_data_sell
 
 def zSortFilterOutliers(series):
+    amount5MinuteSlicesBack = -16
     # Assuming 'data' is a Pandas DataFrame with 'buy_price' and 'sell_price' columns
-    data = pd.DataFrame(series[-24:])
+    data = pd.DataFrame(series[amount5MinuteSlicesBack:])
     print(data)
     # Define outlier threshold using the Z-score method
-    z_threshold = .85
+    z_threshold = 1
 
     # Filter outliers for buy price
     z_scores_buy = np.abs((data['avgLowPrice'] - data['avgLowPrice'].mean()) / data['avgLowPrice'].std())
@@ -117,6 +119,7 @@ def calculateVolatilityAbsMean(lists):
     print(buyVolatility)
     print(sellVolatility)
 
+    return buyVolatility, sellVolatility
 
 def calculateMean(lists):
     buyPriceListMean = lists[0]['avgLowPrice'].mean()
@@ -128,15 +131,55 @@ def calculateMean(lists):
 def calculateGoodFlips():
     margins = calculateMarginCollection(itemList.items)
     items = getTimeSeriesFromList(margins)
+    itemsHandled = []
 
-    
+    for item in items:
+        if(calculateRestriction(item)[0] == "True"):
+            itemsHandled.append((item[0], item[1], item[2]))
+    return itemsHandled
 
+def calculateRestriction(item):
+    margin = item[2][2]
+    buy = item[2][1]
+    sell = item[2][0]
+    series = item[3]
+    volatilityThreshold = 0.002
+    #buyAndSellMultiplier = 0.002
+    thresholdMarginPercentage = 0.0008
 
-
+    outlierFilter = zSortFilterOutliers(series)
+    if(outlierFilter[0].empty or outlierFilter[1].empty):
+        print("not enough data in time series")
+        return ("False")
+    buyAndSellMean = calculateMean(outlierFilter)
+    if (margin < buyAndSellMean[1] * thresholdMarginPercentage):
+        print("small margin")
+        return ("False")
+    buyAndSellVol = calculateVolatilityAbsMean(outlierFilter)
+    if (buyAndSellVol[0] > buyAndSellMean[1] * volatilityThreshold or buyAndSellVol[1] > buyAndSellMean[1] * volatilityThreshold):
+        print("Buy or sell volatile")
+        return ("False")
+    if (buyAndSellMean[0] + buyAndSellVol[0]) < buy:
+        print("Buy deviation high")
+        return ("False")
+    #if (buyAndSellMean[0] - buyAndSellMean[1] * volatilityThreshold) > buy:
+     #   print("Buy deviation low")
+      #  return ("False")
+    if (buyAndSellMean[1] - buyAndSellVol[1]) > sell:
+        print("Sell deviation low")
+        return ("False")
+    if (buyAndSellMean[1] + buyAndSellVol[1]) < sell:
+        print("Sell deviation high")
+        return ("False")
+    return ("True", item)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #series = getTimeSeriesById(13239)
-    #lists = zSortFilterOutliers(series)
+    list = calculateGoodFlips()
+    print(list)
+    #margins = calculateMarginCollection(itemList.items)
+    #items = getTimeSeriesFromList(margins)
+    #series = getTimeSeriesById(13237)
+    #lists = filterOutliers(series)
     #calculateVolatilityAbsMean(lists)
     #calculateMean(lists)
 
